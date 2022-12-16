039ecf47026b0295ac3a0509d021867a9f3ce2dc
Leibniz Gnonsey <frejusgnonsey@Leibnizs-MBP.attlocal.net>
2022-12-16 15:32:15 -0500

    updated wait time using Explicit wait

--- src/main/java/cookiesClicker/cookiesClicker.java
+++ src/main/java/cookiesClicker/cookiesClicker.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 
@@ -19,37 +23,30 @@ public class cookiesClicker {
         driver.get("https://orteil.dashnet.org/cookieclicker/");
 
         long startTime = System.currentTimeMillis();
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        WebElement languageSelect = driver.findElement(By.id("langSelect-EN"));
+
+        WebElement languageSelect = new WebDriverWait(driver, Duration.ofSeconds(5)).
+                until(ExpectedConditions.elementToBeClickable(By.id("langSelect-EN")));
+
+        //WebElement languageSelect = driver.findElement(By.id("langSelect-EN"));
 
 
         languageSelect.click();
-
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
 
         WebElement cookiesClick = driver.findElement(By.id("bigCookie"));
 
 
-        long duration = 0;
-        while ( duration < 60000){
-            cookiesClick.click();
-            long endTime = System.currentTimeMillis();
-            duration = endTime - startTime;
-        }
+//        long duration = 0;
+//        while ( duration < 60000){
+//            cookiesClick.click();
+//            long endTime = System.currentTimeMillis();
+//            duration = endTime - startTime;
+//        }
 
-//        for(int clicksNumber = 0; clicksNumber <= 500; clicksNumber++)
-//            {
-//                cookiesClick.click();
-//            }
+        for(int clicksNumber = 0; clicksNumber <= 300; clicksNumber++)
+            {
+                cookiesClick.click();
+            }
         driver.quit();
     }
 }
